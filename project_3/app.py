559173f2f1f59caf7e3b9e02a11bb85f2f3c707e
Kevin Kuan <kevin.kuan.learning@gmail.com>
2021-06-01 23:09:10 +0800

    all pages

--- project_3/app.py
+++ project_3/app.py
@@ -4,53 +4,63 @@ from fake_useragent import UserAgent
 from pprint import pprint
 from urllib.parse import urljoin
 
-parsed_products = []
-
-ua = UserAgent(verify_ssl=False)
 url = "https://www.walgreens.com/productsearch/v3/products/search"
 
-payload = json.dumps({
-  "p": 1,
-  "s": 72,
-  "view": "allView",
-  "geoTargetEnabled": False,
-  "abtest": [
-    "tier2",
-    "showNewCategories"
-  ],
-  "deviceType": "desktop",
-  "id": [
-    "350006"
-  ],
-  "requestType": "tier3",
-  "sort": "Top Sellers",
-  "couponStoreId": "15196",
-  "storeId": "15196"
-})
+def get_page(page):
+    ua = UserAgent(verify_ssl=False)
 
-headers = {
-  'Content-Type': 'application/json',
-  'User-Agent': ua.random,
-}
+    payload = json.dumps({
+    "p": "{}".format(page),
+    "s": 72,
+    "view": "allView",
+    "geoTargetEnabled": False,
+    "abtest": [
+        "tier2",
+        "showNewCategories"
+    ],
+    "deviceType": "desktop",
+    "id": [
+        "350006"
+    ],
+    "requestType": "tier3",
+    "sort": "Top Sellers",
+    "couponStoreId": "15196",
+    "storeId": "15196"
+    })
 
-response = requests.post(url, headers=headers, data=payload)
-
-j = response.json()
-
-for product in j['products']:
-    prod_info = product['productInfo']
-    pr = {
-        'img': prod_info['imageUrl'],
-        'price': prod_info['priceInfo']['regularPrice'],
-        'id': prod_info['prodId'],
-        'name': prod_info["productName"],
-        'size': prod_info["productSize"],
-        'url': urljoin(base=url, url=prod_info["productURL"]),
+    headers = {
+    'Content-Type': 'application/json',
+    'User-Agent': ua.google,
     }
 
-    pprint(pr)
+    response = requests.post(url, headers=headers, data=payload)
 
-    parsed_products.append(pr)
+    return response.json()
+
+def collect(data):    
+    try:
+        for product in j['products']:
+            prod_info = product['productInfo']
+            pr = {
+                'img': prod_info['imageUrl'],
+                'price': prod_info['priceInfo']['regularPrice'],
+                'id': prod_info['prodId'],
+                'name': prod_info["productName"],
+                'size': prod_info["productSize"],
+                'url': urljoin(base=url, url=prod_info["productURL"]),
+            }
+            parsed_products.append(pr)
+    except KeyError:
+        pass
+
+parsed_products = []
+
+current_page, total_pages = 1, 99
+while current_page <= total_pages:
+    j = get_page(current_page)
+    total_pages = int(j['summary']["totalNumPages"])
+    current_page += 1
+    collect(j)
 
 pprint(parsed_products)
 print(len(parsed_products))
