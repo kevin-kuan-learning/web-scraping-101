61b54541791d820c1f4e1f1766a4be832fc421da
Kevin Kuan <kevin.kuan.learning@gmail.com>
2021-06-01 22:02:09 +0800

    all product

--- project_3/app.py
+++ project_3/app.py
@@ -3,6 +3,8 @@ import json
 from fake_useragent import UserAgent
 from pprint import pprint
 
+parsed_products = []
+
 ua = UserAgent(verify_ssl=False)
 url = "https://www.walgreens.com/productsearch/v3/products/search"
 
@@ -28,13 +30,27 @@ payload = json.dumps({
 headers = {
   'Content-Type': 'application/json',
   'User-Agent': ua.random,
-#   'Cookie': '_abck=B437AD01A0C2E3A36F0C9039A697D3B7~-1~YAAQlF9Ky68P4rB5AQAA+sa0xwUFD+u7LtyG3kL9kFX2sOfJMvE5MkHSegFn7DLWwy4f2vzA2nw+YpifqSIIOmWOakYVx491W94EWeEsW0K1yRc2OGpkbakGEgpt5nAj/SOwLgZsCX8P6WW1YKrCRwXCHASeGmXaC0SzLoMr1iCtyuGc4FnR1lwZKttoTyZlSaRZi/uHt3lVw3R/6fvu9id7r3p8CGEUMoLBYHcBPEtEwP5YbsyjunlGj9nAYn/gaavuX4UY43Yw5mYgPkOHViqi4nP8loHp9Kc0cDzMklUAVD/9wud7zRGVPt/4/u1G8yawyMz/xYHDbb6Q5qWa8u+0NRBUF6JRpsPaGFevyevbhxEgPSL1GL/9qd8=~-1~-1~-1; bm_sz=112B939D90875F071332739B93B5A345~YAAQlF9Ky64P4rB5AQAA+sa0xwvVRsNU5EYuVTunr0+LUaWjDk6E7I3qBw53ruKR5KTIzguiyLf076+I5C6nCXRHHcxjM4N1EFsppWm5YMQGBshywp6Zsu8eKWYhMns1W6IR1kOKoUAwju6a6IuGNj5W3QW0BOUlSdDAYvha7w/U2c59JAJFUCrK3FF+UjpDVaUq; akavpau_walgreens=1622553558~id=56ea1e260b198d7699367f3361daefc0'
 }
 
 response = requests.post(url, headers=headers, data=payload)
 
 j = response.json()
 
-pprint(j)
+for product in j['products']:
+    prod_info = product['productInfo']
+    pr = {
+        'img': prod_info['imageUrl'],
+        'price': prod_info['priceInfo']['regularPrice'],
+        'id': prod_info['prodId'],
+        'name': prod_info["productName"],
+        'size': prod_info["productSize"],
+        'url': prod_info["productURL"],
+    }
+
+    pprint(pr)
+
+    parsed_products.append(pr)
+
+
 
 pass
