7e783f3fd806e6468dcd9b4965eb989575095b19
Kevin Kuan <kevin.kuan.learning@gmail.com>
2021-06-02 13:34:19 +0800

    set header 2

--- splash/app.lua
+++ splash/app.lua
@@ -1,10 +1,13 @@
 function main(splash, args)
     -- splash:set_user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
   
-    headers = {
-      ['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
-    }
-    splash:set_custom_headers(headers)
+    --headers = {
+    --  ['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
+    --}
+    --splash:set_custom_headers(headers)
+    splash:on_request(function(request)
+          request:set_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36')
+    end)
     
     assert(splash:go(args.url))
     assert(splash:wait(1))
