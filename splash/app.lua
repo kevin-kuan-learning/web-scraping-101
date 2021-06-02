0c242fab05f0c254ee74e67eddd489d6d47c1a18
Kevin Kuan <kevin.kuan.learning@gmail.com>
2021-06-02 13:00:40 +0800

    fix bug

--- splash/app.lua
+++ splash/app.lua
@@ -1,10 +1,10 @@
 function main(splash, args)
 
-    assert(splash.go(args.url))
-    assert(splash.wait(1))
+    assert(splash:go(args.url))
+    assert(splash:wait(1))
     return {
-        png = splash.png
-        html = splash.html
+        png = splash:png(),
+        html = splash:html(),
     }
 
 end
