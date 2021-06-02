6cbd8adefb28091975c518f61e8bc1932cefb96a
Kevin Kuan <kevin.kuan.learning@gmail.com>
2021-06-02 13:09:10 +0800

    search

--- splash/app.lua
+++ splash/app.lua
@@ -2,9 +2,17 @@ function main(splash, args)
 
     assert(splash:go(args.url))
     assert(splash:wait(1))
+    input = assert(splash:select("input[name='q']"))
+    
+    input:focus()
+    input:send_text("My User Agent")
+    input:send_keys("<Enter>")
+    assert(splash:wait(1))
+    
+    
     return {
-        png = splash:png(),
-        html = splash:html(),
+      png = splash:png(),
+      html = splash:html(),
     }
-
-end
+  
+  end
